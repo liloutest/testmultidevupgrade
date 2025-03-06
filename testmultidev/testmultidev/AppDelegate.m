3a1318a31ea458a56cb1f7a2b4acc38a5158660f
Lilou <>
2025-03-06 10:53:29 +0800

    22

--- testmultidev/testmultidev/AppDelegate.m
+++ testmultidev/testmultidev/AppDelegate.m
@@ -16,7 +16,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    
+    int i = 0;
     return YES;
 }
 
